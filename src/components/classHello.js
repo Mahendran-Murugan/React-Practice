af7289c81e6356c561847b5fb2ec51b823f9930e
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-05 12:42:05 +0530

    feat: add react project and learn class and functional components

---
+++ src/components/classHello.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class ClassHello extends React.Component{
+    render(){
+        return (
+            <h1>Hello, React from Class!</h1>
+        )
+    }
+}
+
+export default ClassHello
---
+++ src/components/hello.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Hello = () => {
+  return (
+    <h1>Hello, React!</h1>
+  )
+}
